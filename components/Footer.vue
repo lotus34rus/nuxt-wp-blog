34bf310131f9601c7ed508fea585855bd8be2fd2
Dmitriy Mastikov <work.dmitriy34@yandex.ru>
2020-08-05 11:31:10 +0400

    Create simplefooter

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,13 @@
 <template>
-    
+
+  <footer>
+    <div class="container">
+        <div class="name">
+          DM-CODE | Блог веб-разработчика &nbsp;&nbsp; 2020 - {{new Date().getFullYear() }}
+        </div>
+    </div>
+  </footer>
+
 </template>
 
 <script>
@@ -10,3 +18,11 @@
     }),
   }
 </script>
+
+
+<style scoped>
+  footer{
+    border-top: 1px solid lightgray;
+    padding: 15px;
+  }
+</style>
--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="container">ID : {{$route.params.id}}</div>
+  <div class="container">
+      <div class="post-title">
+          <h1 v-html="post.title.rendered"></h1>
+          <p>Опубликовано: <span class="date">{{ new Date(post.date).toLocaleString().split(",")[0] }}</span></p>
+          <p>Категория: {{catName}}</p>
+      </div>
+      <div class="post-content" v-html="post.content.rendered"> </div>
+  </div>
 </template>
 
 
@@ -7,9 +14,36 @@
 export default {
   async asyncData({ $axios, params }) {
     const post = await $axios.$get(
-      "http://dm-code.ru/wp-json/wp/v2/posts/" + params.id + '?_embed'
+      "http://dm-code.ru/wp-json/wp/v2/posts/" + params.id + "?_embed"
     );
     return { post };
   },
+
+  data() {
+    return {
+      thumb: "",
+      catName: "",
+    };
+  },
+  async mounted() {
+    if (this.post.featured_media) {
+      const img = await this.$axios.$get(
+        "http://dm-code.ru/wp-json/wp/v2/media/" + this.post.featured_media
+      );
+      this.thumb = img.guid.rendered;
+    }
+    const cat = await this.$axios.$get(
+      "http://dm-code.ru/wp-json/wp/v2/categories/" + this.post.categories[0]
+    );
+    this.catName = cat.name;
+  },
 };
 </script>
+
+
+<style scoped>
+    .post-title{
+        margin-bottom: 40px;
+        border-bottom: 1px solid lightgray;
+    }
+</style>
