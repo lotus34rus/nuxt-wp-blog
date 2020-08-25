b68cf5459e8ea469b2b394524aa167b2381ea896
Dmitriy Mastikov <work.dmitriy34@yandex.ru>
2020-08-25 11:57:40 +0400

    Add all titles and generate version for deploy

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="container">
     <div class="category-list">
-        <a href=""
-          @click.prevent="filterPost('')"
-        >Все записи ({{posts.length}})</a>
-        <a href=''
-          v-for= "cat of categories"
-          :key = "cat.id"
-          @click.prevent="filterPost(cat.id)"
-        > {{cat.name}} ({{cat.count}}) </a>
+      <a href @click.prevent="filterPost('')">Все записи ({{posts.length}})</a>
+      <a
+        href
+        v-for="cat of categories"
+        :key="cat.id"
+        @click.prevent="filterPost(cat.id)"
+      >{{cat.name}} ({{cat.count}})</a>
     </div>
 
     <div class="blog-list">
-      <Card v-for="post of posts" :filter = 'filter' :key="post.id" :post="post" />
+      <Card v-for="post of posts" :filter="filter" :key="post.id" :post="post" />
     </div>
   </div>
 </template>
@@ -21,13 +20,18 @@
 export default {
   data() {
     return {
-      filter: ''
+      filter: "",
+    };
+  },
+  head() {
+    return {
+      title: "Блог Веб-разработчика | DM-CODE.RU",
     };
   },
   methods: {
-    filterPost(catId){
-      this.filter = catId
-    }
+    filterPost(catId) {
+      this.filter = catId;
+    },
   },
   async fetch({ store }) {
     if (store.getters["posts/getPosts"].length === 0) {
@@ -40,30 +44,30 @@ export default {
     posts() {
       return this.$store.getters["posts/getPosts"];
     },
-    categories(){
+    categories() {
       return this.$store.getters["categories/getCategories"];
-    }
+    },
   },
 };
 </script>
 
 
 <style scoped>
-.blog-list{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
+.blog-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
 }
-.category-list{
+.category-list {
   padding: 0px 0px 20px 0;
 }
-.category-list a{
+.category-list a {
   font-size: 24px;
   margin-right: 10px;
   color: black;
   text-decoration: none;
 }
-.category-list a:hover{
+.category-list a:hover {
   color: grey;
 }
 </style>
--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,29 +1,45 @@
 <template>
-    <div class="container">
-        <p>
-            Если у вас есть вопросы, предложения или полезная информация, вы всегда можете меня найти на следующих реусрсах:
-        </p>
+  <div class="container">
+    <p>Если у вас есть вопросы, предложения или полезная информация, вы всегда можете меня найти на следующих реусрсах:</p>
 
-        <p><b>Персональный сайт:</b> <a href="http://dm-code.ru">ТЫК</a></p>
-        <p><b>GitHub:</b> <a href="https://github.com/lotus34rus">ТЫК</a></p>
-        <p><b>Instagram:</b> <a href="https://www.instagram.com/work.dmitriy34/">ТЫК</a> </p>
-
-    </div>
+    <p>
+      <b>Персональный сайт:</b>
+      <a href="http://dm-code.ru">ТЫК</a>
+    </p>
+    <p>
+      <b>GitHub:</b>
+      <a href="https://github.com/lotus34rus">ТЫК</a>
+    </p>
+    <p>
+      <b>Instagram:</b>
+      <a href="https://www.instagram.com/work.dmitriy34/">ТЫК</a>
+    </p>
+  </div>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      title: "Блог Веб-разработчика | DM-CODE.RU",
+    };
+  },
+};
+</script>
+
 <style scoped>
-    p{
-        font-size: 22px;
-        width: 70%;
-        text-align: center;
-        line-height: 30px;
-        margin-bottom: 10px;
-    }
-    .container{
-        max-width: 600px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
+p {
+  font-size: 22px;
+  width: 70%;
+  text-align: center;
+  line-height: 30px;
+  margin-bottom: 10px;
+}
+.container {
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 </style>
